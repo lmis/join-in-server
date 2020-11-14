420d3533070ce97f19433e5e9d9e016e58fd1e2e
lmis <lionel.miserez@gmail.com>
2020-11-14 18:43:10 +0100

    Make cors match more generally

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,44 @@
 import express from "express";
-import path from "path";
 import bodyParser from "body-parser";
 import socketIO, { Socket } from "socket.io";
+import cors from "cors";
 
 const port = 8000;
 
 const server = express()
+  .use(cors({ origin: /\.csb\.app$/ }))
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: true }))
-  .get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../", "public/error.html"));
-  })
   .listen(port, () => console.log(`Listening on port ${port}`));
 
 const socketIds = new Set<string>();
-socketIO(server)
-  .on("connection", (socket: Socket) => {
-    const { id } = socket;
-    console.log("Socket connected", JSON.stringify(socket));
+const maxNumberUsers = 20;
+(socketIO as any)(server).on("connection", (socket: Socket) => {
+  const { id } = socket;
+  console.log(`Socket #${socketIds.size + 1} conntected. Id: ${id}`);
+  if (socketIds.size === maxNumberUsers) {
+    socket.emit("max-users-reached");
+    socket.disconnect();
+    return;
+  } else {
+    socket.emit("hello-client", { id });
+  }
 
-    if (!socketIds.has(id)) {
-      const userIds: string[] = [...socketIds];
-      socket.broadcast.emit("add-users", {
-        userIds: [socket.id]
-      });
-      socket.emit("add-users", {
-        userIds
-      });
-      socketIds.add(id);
-    }
-  })
-  .on("disconnect", (socket: Socket) => {
+  if (!socketIds.has(id)) {
+    const userIds: string[] = [...socketIds];
+    socket.broadcast.emit("add-users", {
+      userIds: [socket.id]
+    });
+    socket.emit("add-users", {
+      userIds
+    });
+    socketIds.add(id);
+  }
+  socket.on("disconnect", () => {
     const { id } = socket;
-    console.log("Socket disconnected", JSON.stringify(socket));
+    console.log(`Socket ${id} disconnected.`);
     socket.broadcast.emit("remove-user", {
       userId: id
     });
   });
+});
